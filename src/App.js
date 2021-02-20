8342d93bcd08a15343792ba2b2de0b1c4bab3987
Esteban <estebaanbisocoli@gmail.com>
2021-02-20 15:38:35 -0300

    video support

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import Hls from "hls.js"
 function App() {
+  const [src, setSrc] = useState()
+  const videoRef = useRef()
+  useEffect(()=> {
+    fetch("https://cyclic-alpine-mapusaurus.glitch.me", {method: "GET"}).then((res) => {
+      return res.json()
+    }).then((res) => setSrc(res.url))
+  }, [])
+
+  useEffect(()=> {
+    if(src) {
+      if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(videoRef.current);
+        videoRef.current.controls = true
+      }
+      else if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+        videoRef.current.src = src;
+      }
+    }
+  }, [src])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        minHeight: "100vh",
+        minWidth: "100vw",
+        backgroundColor: "yellow",
+        display: "flex",
+        alignItems: "center",
+        boxSizing: "border-box"
+      }}
+    >
+      <video ref={videoRef} style={{width: "100%", maxHeight: "95vh"}} ></video>
     </div>
   );
 }
